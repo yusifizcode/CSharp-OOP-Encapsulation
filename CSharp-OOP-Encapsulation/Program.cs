61df4d8f6eab95ffd2994206898276fa8f09c671
yusifizcode <5ba0gh2@code.edu.az>
2022-03-29 03:33:22 +0400

    c3 oop encapsulation homework

---
+++ CSharp-OOP-Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_OOP_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Istifadeci adinizi daxil edin: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Parolu daxil edin: ");
+            string pass = Console.ReadLine();
+
+            User user = new User(userName);
+            user.Password = pass;
+            Console.WriteLine($"Istifadeci adi: {user.UserName} - password: {user.Password}");
+        }
+    }
+}
---
+++ CSharp-OOP-Encapsulation/User.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP_Encapsulation
+{
+    internal class User
+    {
+        public User(string username)
+        {
+            this.UserName = username;
+        }
+        public string _userName;
+        private string _password;
+
+        public string UserName { get { return this._userName; }  set 
+            {
+                if (value.Length>6 && value.Length<25)
+                {
+                    _userName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Istifadeci adinin uzunlugu 6-dan boyuk 25-den kicik olmalidir.");
+                }
+            
+            
+            } }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+
+                if (value.Length > 8 && value.Length < 25 && HasDigit(value) && HasLower(value) && HasUpper(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Parolun uzunlugu 8-den boyuk 25-den kicik olmalidir, icerisinde boyuk herf kicik herf ve reqem olmalidir.");
+                }
+
+            }
+        }
+
+        public bool HasDigit(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasUpper(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasLower(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLower(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
